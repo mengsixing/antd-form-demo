2da0028682e32a79a99950a8f6202ce2c0a03b77
yhlben <yinhengli@sina.com>
2018-04-11 18:19:54 +0800

    add antd-form and rc-form

---
+++ Test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import hoistStatics from 'hoist-non-react-statics';
+
+class Form extends React.Components{
+  render(){
+    return (
+      <div>
+        <h1>title</h1>
+      </div>
+    )
+  }
+}
+
+class FormItem extends React.Components{
+  render(){
+    return (
+      <div>
+        <div>form item</div>
+      </div>
+    )
+  }
+}
