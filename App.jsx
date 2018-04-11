fff1f715e255c9018f9d14512da4c6f4bcc15bd9
尹珩力 <yinhengli@sina.com>
2018-04-11 08:31:42 +0800

    init

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import HorizontalLoginForm from './HorizontalLoginForm'
+
+
+
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>hello form</h1>
+                <HorizontalLoginForm />
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ HorizontalLoginForm.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Form, Icon, Input, Button } from 'antd';
+const FormItem = Form.Item;
+
+
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+class HorizontalLoginForm extends React.Component {
+  constructor(){
+      super();
+      this.handleSubmit=this.handleSubmit.bind(this);
+  }
+  componentDidMount() {
+    this.props.form.validateFields();
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.props.form.setFieldsValue({'userName':'张三'}));
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+  render() {
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+
+    // Only show error after a field is touched.
+    const userNameError = isFieldTouched('userName') && getFieldError('userName');
+    const passwordError = isFieldTouched('password') && getFieldError('password');
+    return (
+      <Form layout="inline" onSubmit={this.handleSubmit}>
+        <FormItem
+          validateStatus={userNameError ? 'error' : ''}
+          help={userNameError || ''}
+        >
+          {getFieldDecorator('userName', {
+            rules: [{ required: true,len:3 }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+          )}
+        </FormItem>
+        <FormItem
+          validateStatus={passwordError ? 'error' : ''}
+          help={passwordError || ''}
+        >
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={hasErrors(getFieldsError())}
+          >
+            Log in
+          </Button>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+const WrappedHorizontalLoginForm = Form.create()(HorizontalLoginForm);
+
+export default WrappedHorizontalLoginForm;
